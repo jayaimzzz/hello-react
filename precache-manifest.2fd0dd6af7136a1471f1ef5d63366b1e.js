865a73b1e554300eaf0ca8a060fcbcd85326591b
James Horton <jameshorton@gmail.com>
2018-11-07 16:09:39 -0500

    Updates

---
+++ precache-manifest.2fd0dd6af7136a1471f1ef5d63366b1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ac5259eac6c393b99aa",
+    "url": "/hello-react/static/js/runtime~main.2ac5259e.js"
+  },
+  {
+    "revision": "ed4979ef942f6e96fa14",
+    "url": "/hello-react/static/js/main.ed4979ef.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/hello-react/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ed4979ef942f6e96fa14",
+    "url": "/hello-react/static/css/main.75c379fc.chunk.css"
+  },
+  {
+    "revision": "fcc65a0474887c74542a699bf6565cae",
+    "url": "/hello-react/index.html"
+  }
+];
